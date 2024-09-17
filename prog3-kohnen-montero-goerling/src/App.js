22fcfc6a40d58fbe7dcff5cdd0e1881b5e8b80b4
camilakohnen <ckohnen@udesa.edu.ar>
2024-09-17 16:04:44 -0500

    Punto de navegación completo

--- prog3-kohnen-montero-goerling/src/App.js
+++ prog3-kohnen-montero-goerling/src/App.js
@@ -1,6 +1,27 @@
+import Home from "../src/screens/Home";
+import {Route, Switch} from 'react-router-dom'
+import NotFound from "./screens/NotFound";
+import Details from "./screens/Details";
+import Navbar from "./components/Header";
+import Favoritos from "./screens/Favoritos";
+import Novedades from "./components/VerTodos/novedades";
+import Clasicos from "./components/VerTodos/clasicos";
+import Footer from "./components/Footer";
+
 function App(props){
+  console.log('Estas son las props de la aplicacion');
   return (
-    <> 
+    <>
+      <Navbar/>
+      <Switch>
+        <Route path='/' exact={true} component={Home}></Route>
+        <Route path='/favoritos' component={Favoritos}></Route>
+        <Route path='/novedades' component={Novedades}></Route>
+        <Route path='/clasicos' component={Clasicos}></Route>
+        <Route path='/detalle/id/:id' component={Details}></Route>
+        <Route path='' component={NotFound}></Route>
+      </Switch>
+      <Footer/>
     </>
   )
 }
